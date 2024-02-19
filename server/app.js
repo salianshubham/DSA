20a1b1ee3b0d621af3b63938a44e0fd01b7d2a01
shubham <shubhamsalian46@gmail.com>
2024-02-19 09:36:06 +0530

    19-02-2024

--- server/app.js
+++ server/app.js
@@ -1,12 +1,28 @@
 const express = require("express")
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
+
+
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send(`Hello from Server`);
 })
 
 
-app.listen(5000, () => {
-    console.log("server ")
+app.post('/DSA/array', (req, res) => {
+    const number = req.body.number
+    const arr = req.body.arr;
+    console.log("AA", number)
+    console.log("arr", arr)
+    res.json({
+        status: 200,
+        message: "Success",
+        data: number,arr
+    });
 })
-console.log("aa")
+
+app.listen(5000, () => {
+    console.log("server running in port 5000")
+})
